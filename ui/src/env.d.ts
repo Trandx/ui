323573e3ce66ebd5f1018f12b58f7af40f098bf9
Trandx <tsabimmo@gmail.com>
2024-07-22 23:58:01 +0100

    env config

--- ui/src/env.d.ts
+++ ui/src/env.d.ts
@@ -9,8 +9,6 @@ interface ImportMetaEnv {
   readonly VITE_APP_API_DOMAINE: string;
   readonly VITE_APP_API_PORT: number;
   readonly VITE_APP_API_BASE_URL: string;
-  readonly VITE_APP_API_RESSOURCE: string;
-  readonly VITE_APP_API_FAKER_RESSOURCE: string;
 }
 
 interface ImportMeta {
